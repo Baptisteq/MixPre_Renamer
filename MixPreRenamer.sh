d5573e48e8bdef96262e992f44b810b19bc92a90
Baptiste Quemener <quemeneb@gmail.com>
2020-05-07 12:18:40 +0200

    WIP

--- MixPreRenamer.sh
+++ MixPreRenamer.sh
@@ -9,6 +9,10 @@ echo "ERROR:$ERROR"
 exit
 }
 
+#--Generate log file---#
+LOG=Log.txt
+[ -f $LOG ] && echo "==New Batch == $(Date)">>$LOG|| echo "Original file name ==> New filename ==> Date">$LOG
+
 #----InputParameter
 
 INPUTDIR=$1
@@ -16,8 +20,8 @@ INPUTDIR=$1
 #----Input Validation
 [ -d $INPUTDIR ] && echo "input dir:$INPUTDIR is a valid folder" || usage "input dir:$INPUTDIR is not a valid dir"
 
-#----List all .WAV files contained in INPUTDIR----#
-LISTOFWAV=$(find $INPUTDIR | grep ".*\.WAV")
+#----List all .WAV files contained in INPUTDIR (! not recursively)----#
+LISTOFWAV=$(find $INPUTDIR -maxdepth 1 | grep ".*\.WAV")
 # if no found file => exit
 [[ "$LISTOFWAV" == "" ]] && usage "No WAV files have been found in this dir:$INPUTDIR" || echo "$LISTOFWAV"
 
@@ -27,39 +31,44 @@ do
   WAVDISKNAME=$(echo "$WAV" | sed "s/\/cygdrive\/\([a-z]\)\/.*/\1/" | tr "[:lower:]" "[:upper:]")
   WAVDISKNAME="$WAVDISKNAME"":"
   WAVWINPATH=$(echo "$WAV" | sed "s/\/cygdrive\/[a-z]/$WAVDISKNAME/" )
-  
-  # Get old original file name
-  OLDFILENAME=$(echo "$WAV" | sed "s/^.*\/\(.*\)\.WAV$/\1/")  
-  # new  base file name based on sNOTE
+  echo "wavwinpath:$WAVWINPATH"
+  #Get Original FileName (the one gernate by the MixPre)
+  ORIGINALFILENAME=$(ffprobe -loglevel -8 -show_entries "format_tags" "$WAVWINPATH" | grep "sFILENAME=" | tr -d "\r" )
+  ORIGINALFILENAME=${ORIGINALFILENAME:10:(-4)}
+  echo "original filename:$ORIGINALFILENAME"
+  #Get Original TakeNumber
+  ORIGINALTAKENUMBER=$(echo "$ORIGINALFILENAME" | sed "s/.*-\([0-9]*\)$/\1/")
+  echo "original take number:$ORIGINALTAKENUMBER"
+  # new  base file name based on sNOTE (snote must not over reach 84 characrters max)
   NEWFILENAME=$(ffprobe -loglevel -8 -show_entries "format_tags" "$WAVWINPATH" | grep "sNOTE=" | tr -d "\r" | sed "s/[^a-zA-Z0-9]/_/g" | tr " " "_" | tr "[:upper:]" "[:lower:]" | sed "s/_*$//" )
   NEWFILENAME=${NEWFILENAME:6}
-  [[ $NEWFILENAME == "" ]] && NEWFILENAME="$OLDFILENAME"
-    
-  # Format value (stereo mono), Bitdepth value, samplerate value
-  FORMATVALUE=$(ffprobe -loglevel -8 -show_entries "format_tags" "$WAVWINPATH" | grep "TAG:coding_history=" | tr -d "\r" | sed "s/.*,M=\([^,]*\),.*/\1/" )
-    case $FORMATVALUE in
-      "mono")FORMATSTR="M";;
-    "stereo")FORMATSTR="ST";;
-           *)FORMATSTR="X"  
-  esac
-  BITDEPTHVALUE=$(ffprobe -loglevel -8 -show_entries "format_tags" "$WAVWINPATH" | grep "TAG:coding_history=" | tr -d "\r" | sed "s/.*,W=\([0-9][0-9]\),.*/\1/" )
-  SAMPLERATEVALUE=$(ffprobe -loglevel -8 -show_entries "format_tags" "$WAVWINPATH" | grep "TAG:coding_history=" | tr -d "\r" | sed "s/.*,F=\([0-9]*\),.*/\1/" | tr -d "0" )
   
-  # concatenate
-  OUTPUTFILENAME="$INPUTDIR""/""$NEWFILENAME"_"$FORMATSTR""$BITDEPTHVALUE""$SAMPLERATEVALUE"".WAV"
-
-  # check for already existing file incrementing value "(_001.WAV) 
-  if [ -f $OUTPUTFILENAME ]; then
-    if [[ "$OUTPUTFILENAME" =~ .*_\([0-9][0-9][0-9]\)\.WAV ]]; then
-      ORIGINALFILENUMBER=${BASH_REMATCH[1]}
-      ((NEWFILENUMBER=ORIGINALFILENUMBER+1))
-      NEWFILENUMBER=$(printf "%03d\n" $NEWFILENUMBER)
-      OUTPUTFILENAME="$INPUTDIR""/""$NEWFILENAME"_"$FORMATSTR""$BITDEPTHVALUE""$SAMPLERATEVALUE""_""NEWFILENUMBER"".WAV"
-      else
-      OUTPUTFILENAME="$INPUTDIR""/""$NEWFILENAME"_"$FORMATSTR""$BITDEPTHVALUE""$SAMPLERATEVALUE""_002"".WAV"
-    fi
+  # Define if newfile name is applied or original filename is preserved  
+  if [[ $NEWFILENAME == "" ]]; then
+    NEWFILENAME="$ORIGINALFILENAME"
+    else
+    # get Format value (stereo mono), Bitdepth value, samplerate value
+    FORMATVALUE=$(ffprobe -loglevel -8 -show_entries "format_tags" "$WAVWINPATH" | grep "TAG:coding_history=" | tr -d "\r" | sed "s/.*,M=\([^,]*\),.*/\1/" )
+      case $FORMATVALUE in
+        "mono")FORMATSTR="M";;
+      "stereo")FORMATSTR="ST";;
+             *)FORMATSTR="X"  
+      esac
+    BITDEPTHVALUE=$(ffprobe -loglevel -8 -show_entries "format_tags" "$WAVWINPATH" | grep "TAG:coding_history=" | tr -d "\r" | sed "s/.*,W=\([0-9][0-9]\),.*/\1/" )
+    SAMPLERATEVALUE=$(ffprobe -loglevel -8 -show_entries "format_tags" "$WAVWINPATH" | grep "TAG:coding_history=" | tr -d "\r" | sed "s/.*,F=\([0-9]*\),.*/\1/" | tr -d "0" )
+    # new file name is applied concatenante sNOTE + OriginalTakeNumber + BitDepthSR
+    NEWFILENAME="$NEWFILENAME""_T""$ORIGINALTAKENUMBER""_""$FORMATSTR""$BITDEPTHVALUE""$SAMPLERATEVALUE"
+  echo "NewFIleName:$NEWFILENAME"
   fi
-  
+    
+  # concatenate input dir
+  OUTPUTFILENAME="$INPUTDIR""/""$NEWFILENAME"".WAV"
+  echo "output path:$OUTPUTFILENAME"
+ 
   #rename
   mv $WAV $OUTPUTFILENAME
+  #log#
+  echo -e "$ORIGINALFILENAME"" ==> ""$NEWFILENAME"" ==> ""$(date)\r" >>$LOG
 done
+
+exit
